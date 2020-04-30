c3ffb37e8db0c3f732f43f90939088f5778badad
sumanthmunikoti <sumanthmunikoti1781989@gmail.com>
2020-04-30 00:13:04 -0400

    React connected to express JS

--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import socketIOClient from "socket.io-client";
 import ReactDOM from "react-dom";
 
-import { ControlledEditor } from "@monaco-editor/react";
+import {ControlledEditor} from "@monaco-editor/react";
 
+const ENDPOINT = 'http://localhost:8080'
 const BAD_WORD = "eval";
-const WORNING_MESSAGE = " <- hey man, what's this?";
+const WARNING_MESSAGE = " <- hey man, what's this?";
 
 function App() {
-  const [value, setValue] = useState("// write javascript code here \n");
+    const [response, setResponse] = useState("// write javascript code here \n");
 
-  const handleEditorChange = (ev, value) => {
-    setValue(
-      value.includes(BAD_WORD) && !value.includes(WORNING_MESSAGE)
-        ? value.replace(BAD_WORD, BAD_WORD + WORNING_MESSAGE)
-        : value.includes(WORNING_MESSAGE) && !value.includes(BAD_WORD)
-        ? value.replace(WORNING_MESSAGE, "")
-        : value
+    const handleEditorChange = (ev, value) => {
+        setResponse(
+            value.includes(BAD_WORD) && !value.includes(WARNING_MESSAGE)
+            ? value.replace(BAD_WORD, BAD_WORD + WARNING_MESSAGE)
+            : value.includes(WARNING_MESSAGE) && !value.includes(BAD_WORD)
+              ? value.replace(WARNING_MESSAGE, "")
+              : value
+        );
+    };
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("FromAPI", data => {
+            setResponse(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            <ControlledEditor
+                height="90vh"
+                value={response}
+                onChange={handleEditorChange}
+                language="javascript"
+            />
+            <p>
+                It's <time dateTime={response}>{response}</time>
+            </p>
+        </div>
     );
-  };
-
-  return (
-    <ControlledEditor
-      height="90vh"
-      value={value}
-      onChange={handleEditorChange}
-      language="javascript"
-    />
-  );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App/>, rootElement);
